c589e55abba36b79bfe13ddd30c32de48dd9d46c
Ameed-salameh <ameed.salame123@gmail.com>
2026-03-19 23:37:17 +0200

    add class book

---
+++ Library Management System/book .cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System
+{
+    internal class book
+    {
+        public int id;
+        public string title;
+        public string author;
+        public double price;
+        public string IsBorrowed;
+
+
+
+        public void display()
+        {
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Author: " + author);
+            Console.WriteLine("Price: " + price);
+            Console.WriteLine("Is Borrowed: " + IsBorrowed);
+        }
+
+    }
+}
