daf4b52c66d15a6e9dc76e2ffd7a4a79359ec225
Ameed-salameh <ameed.salame123@gmail.com>
2026-03-20 01:25:28 +0200

    Done Transaction

--- Library Management System/Transaction .cs
+++ Library Management System/Transaction .cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Library_Management_System
 {
+
+    enum TransactionType
+    {
+        Borrow,
+        Return
+    }
+
+
     internal class Transaction
     {
+        public int Id;
+        public string BookTitle;
+        public string MemberName;
+        public DateTime Date;
+        public TransactionType Type;
+
+
+        public Transaction(int id, string bookTitle, string memberName, TransactionType type)
+        {
+            Id = id;
+            BookTitle = bookTitle;
+            MemberName = memberName;
+            Type = type;
+            Date = DateTime.Now; 
+        }
+
+
+
     }
 }
