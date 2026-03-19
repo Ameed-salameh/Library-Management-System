d7d2b3cc47069ffa39c3f9279d780519852102d7
Ameed-salameh <ameed.salame123@gmail.com>
2026-03-20 01:13:46 +0200

    add IFineCalculator

---
+++ Library Management System/InIFineCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System
+{
+    internal interface InIFineCalculator
+    {
+        double CalculateFine(int daysLate);
+
+    }
+}
