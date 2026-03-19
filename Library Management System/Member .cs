fe0d512d0a097f5bb70f1917f34c4077255798ff
Ameed-salameh <ameed.salame123@gmail.com>
2026-03-19 23:48:36 +0200

    Member

--- Library Management System/Member .cs
+++ Library Management System/Member .cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Library_Management_System
 {
-    internal class Member
+    internal class Member:book
     {
+        public int id;
+        public string name;
+        public int phone;
+        string[] borrowedBooks = new string[2];
+
+
 
     }
 }
